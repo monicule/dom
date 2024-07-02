47bf7c525559bed0d1c44c2c1efe0ca25810702d
Monika <laiskutis.monikai@gmail.com>
2024-07-02 10:37:43 +0300

    innerText

--- about-us/about.js
+++ about-us/about.js
@@ -13,7 +13,7 @@ console.clear();
 const ulDOM = document.getElementsByTagName('ul');
 console.log(ulDOM);
 
-const liDOM = ulDOM[1].getElementsByTagName('li');
+const liDOM = ulDOM[0].getElementsByTagName('li');
 
 for (let i = 0; i < liDOM.length; i++) {
     console.log(liDOM[i].innerText);
@@ -21,3 +21,18 @@ for (let i = 0; i < liDOM.length; i++) {
 
 
 // elementas.innerText = '145621';
+
+for (let i = 0; i < liDOM.length; i++) {
+    console.log(liDOM[i].innerText + ' 111');
+}
+
+for (let i = 0; i < liDOM.length; i++) {
+    for (let y = 1; y < liDOM.length + 1; y++) {
+        console.log( y + ') ' + liDOM[i++].innerText);
+}
+
+}
+
+console.log(h1DOM.innerText.toString());
+
+
